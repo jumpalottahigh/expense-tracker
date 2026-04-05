3217ff68e08c5ca69928ff155c4444566854198d
Georgi Yanev <georgiyanev.gy@gmail.com>
2026-04-05 21:32:45 +0300

    Update icon, fix iOS status bar color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
-import { ThemeProvider } from '../context/ThemeContext'
+import { ThemeProvider, useTheme } from '../context/ThemeContext'
 import { SupabaseProvider } from '../context/SupabaseContext'
 import Head from 'next/head'
+
+function ThemeMeta() {
+  const { isDarkMode } = useTheme()
+  return (
+    <Head>
+      <meta name="theme-color" content={isDarkMode ? '#121212' : '#f0f2f5'} />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+    </Head>
+  )
+}
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -12,7 +22,10 @@ function MyApp({ Component, pageProps }) {
             name="viewport"
             content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"
           />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         </Head>
+        <ThemeMeta />
         <Component {...pageProps} />
       </ThemeProvider>
     </SupabaseProvider>
